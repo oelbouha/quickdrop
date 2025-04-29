7b2d2d6bba8bda19cdcd6f19c81632b8477b1ef1
outman elbouhali <outmanelbouhali5@gmail.com>
2025-04-29 12:49:59 -0400

    updating ui: change the old active shipment card with the new ui

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color background = Color(0xFF111827); // Main background
+  static const Color background = Color(0xFF0A0E17); // Main background
   static const Color blue = Color(0xFF4d6bfe); // Active color
   static const Color headingText = Color(0xFFffffff); // Heading text
   static const Color lessImportant = Color(0xFFB3B3B3);
@@ -9,8 +9,9 @@ class AppColors {
   static const Color succes = Color(0xFF10B981);
   static const Color warning = Color(0xFFF59E0B);
   static const Color error = Color(0xFFEF4444);
-  static const Color cardBackground = Color(0xFF1F2937);
-  static const Color barColor = Color(0xFF1F2937);
+  static const Color cardBackground = Color(0xFF161B26);
+  static const Color cardBorder = Color(0xFF1E2533);
+  static const Color barColor = Color(0xFF161B26);
   static const Color white = Color.fromARGB(255, 255, 255, 255);
   static const Color tabBarBorderColor = Color.fromARGB(255, 92, 89, 89);
   static const Color requestButton = Color(0xFFd3dffb);
