4f6cba3b16e94683ee4b3daea6e8eee1f4594394
outman elbouhali <outmanelbouhali5@gmail.com>
2025-06-19 00:58:23 -0400

    finishing shipment card and trip card ui :

--- lib/core/widgets/floatingActionButton.dart
+++ lib/core/widgets/floatingActionButton.dart
@@ -21,7 +21,8 @@ class FloatButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
+        duration: const Duration(milliseconds: 350),
+        curve: Curves.easeInOutCubic,
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         decoration: BoxDecoration(
           color: AppColors.blue700,
@@ -38,23 +39,50 @@ class FloatButton extends StatelessWidget {
           ],
         ),
         child: AnimatedSize(
-          duration: const Duration(milliseconds: 400),
+          duration: const Duration(milliseconds: 350),
           curve: Curves.easeInOut,
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              const Icon(Icons.add, color: Colors.white, size: 28),
-              if (expanded) ...[
-                const SizedBox(width: 8),
-                Text(
-                  hintText,
-                  style: const  TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
+              AnimatedScale(
+                duration: const Duration(milliseconds: 200),
+                  scale: expanded ? 1.0 : 0.9,
+                  child: const Icon(Icons.add, color: Colors.white, size: 28),
                 ),
-              ],
+                AnimatedContainer(
+                  duration: const Duration(microseconds: 350),
+                  curve: Curves.easeInOutCubic,
+                  width: expanded? null : 0,
+                  child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 250),
+                  opacity: expanded ? 1.0 : 0.0,
+                  child: expanded
+                      ? Padding(
+                          padding: const EdgeInsets.only(left: 8),
+                          child: Text(
+                            hintText,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                ),
+
+                ),
+              // if (expanded) ...[
+              //   const SizedBox(width: 8),
+              //   Text(
+              //     hintText,
+              //     style: const  TextStyle(
+              //       color: Colors.white,
+              //       fontSize: 16,
+              //       fontWeight: FontWeight.w500,
+              //     ),
+              //   ),
+              // ],
             ],
           ),
         ),
