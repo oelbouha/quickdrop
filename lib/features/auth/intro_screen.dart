e4cfa83446669880e7ec538726060da7e8160490
oelbouha <outmanelbouhali5@gmail.com>
2025-07-24 11:02:53 +0100

    updating welcome screen

--- lib/features/auth/intro_screen.dart
+++ lib/features/auth/intro_screen.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quickdrop_app/features/navigation/bottom_nav_bar.dart';
-import 'package:quickdrop_app/features/auth/login_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
-import 'package:quickdrop_app/features/models/user_model.dart';
 import 'package:quickdrop_app/core/utils/imports.dart';
 import 'package:flutter/services.dart'; 
-import 'package:quickdrop_app/core/widgets/auth_button.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
@@ -22,127 +13,122 @@ class _IntroScreenState extends State<IntroScreen> {
   bool _isLoginLoading = false;
   bool _isSignUpLoading = false;
 
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/back.jpg'),
-              fit: BoxFit.cover,
-            ),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFBFDBFE), // from-blue-200
+              Color(0xFF93C5FD), // via-blue-300  
+              Color(0xFF60A5FA), // to-blue-400
+            ],
+            stops: [0.0, 0.5, 1.0],
           ),
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.black.withOpacity(0.2),
-                  Colors.black.withOpacity(0.1),
-                ],
-              ),
-            ),
-          
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Header Section
-                  const Column(
-                    children: [
-                      const SizedBox(height: 32),
-                      Text(
-                        'QUICKDROP',
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Header Section
+                const Column(
+                  children: [
+                    SizedBox(height: 32),
+                    Text(
+                      'QUICKDROP',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
                       ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Connect. Ship. Deliver.',
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.white, 
-                        ),
-                      ),
-                    ],
-                  ),
-                  
-                  const SizedBox(height: 32),
-                  
-                  // Spacer for visual balance
-                  Container(
-                    width: double.infinity,
-                    height: 192,
-                  ),
-                  
-                  // const SizedBox(height: 48),
-                  
-                  // Buttons Section
-                  const Spacer(),
-                  Column(
-                    children: [
-                      // Sign In Button
-                      LoginButton(
-                        hintText: "Sign in",
-                        backgroundColor: AppColors.white,
-                        textColor: AppColors.blue700,
-                        onPressed: () {
-                          setState(() {
-                            _isLoginLoading = true;
-                          });
-                          context.pushNamed('login');
-                          setState(() {
-                            _isLoginLoading = false;
-                          });
-                        },
-                        isLoading: _isLoginLoading,
-                        radius: 60,
-                      ),
-                      
-                      const SizedBox(height: 12),
-                      
-                      LoginButton(
-                        hintText: "Sign up",
-                        onPressed: () {
-                          setState(() {
-                            _isSignUpLoading = true;
-                          });
-                          context.pushNamed('signup');
-                          setState(() {
-                            _isSignUpLoading = false;
-                          });
-                        },
-                        backgroundColor: AppColors.appBarIcons,
-                        isLoading: _isSignUpLoading,
-                        radius: 60,
-                      ),
-                    ],
-                  ),
-                  
-                  const SizedBox(height: 32),
-                  
-                  // Bottom Indicator
-                  const Spacer(),
-                  Container(
-                    width: 100,
-                    height: 4,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(2),
                     ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Connect. Ship. Deliver.',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.black, 
+                      ),
+                    ),
+                  ],
+                ),
+                
+                const SizedBox(height: 32),
+                
+                // Spacer for visual balance
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  decoration: const BoxDecoration(),
+                  child: Image.asset('assets/images/ani.png', 
+                    fit: BoxFit.cover,
                   ),
-                ],
-              ),
+                ),
+                
+                // Buttons Section
+                const Spacer(),
+                Column(
+                  children: [
+                    // Sign In Button
+                    LoginButton(
+                      hintText: "Sign in",
+                      backgroundColor: AppColors.white,
+                      textColor: AppColors.blue700,
+                      onPressed: () {
+                        setState(() {
+                          _isLoginLoading = true;
+                        });
+                        context.pushNamed('login');
+                        setState(() {
+                          _isLoginLoading = false;
+                        });
+                      },
+                      isLoading: _isLoginLoading,
+                      radius: 60,
+                    ),
+                    
+                    const SizedBox(height: 12),
+                    
+                    LoginButton(
+                      hintText: "Sign up",
+                      onPressed: () {
+                        setState(() {
+                          _isSignUpLoading = true;
+                        });
+                        context.pushNamed('signup');
+                        setState(() {
+                          _isSignUpLoading = false;
+                        });
+                      },
+                      backgroundColor: AppColors.appBarIcons,
+                      isLoading: _isSignUpLoading,
+                      radius: 60,
+                    ),
+                  ],
+                ),
+                
+                const SizedBox(height: 32),
+                
+                // Bottom Indicator
+                const Spacer(),
+                Container(
+                  width: 100,
+                  height: 4,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+              ],
             ),
           ),
-        )),
+        ),
+      ),
     );
   }
 }
